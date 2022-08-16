224ef1bc32da1f1fa7d1fec1dccee816952cf8e0
MaximusPhilipsChan <108403458+Yeeun888@users.noreply.github.com>
2022-08-16 10:08:13 +0700

    zzz

--- codecademyCppBasics/test.cpp
+++ codecademyCppBasics/test.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
-#include <tuple>
+#include <stdlib.h>
 
 using namespace std;
 
-tuple<int, int, int> getCurrency() {
-
-int pesos, reais, soles;
-
-cout << "Enter number of Colombian Pesos:";
-cin >> pesos;
-
-cout << "Enter number of Brazilian Reais:";
-cin >> reais;
-
-cout << "Enter number of Peruvian Soles:";
-cin >> soles;
-
-return make_tuple(pesos, reais, soles);
-
-}
-
 int main(){
-  auto [x,y,z]  = getCurrency();
+  srand(time(NULL));
 
+  int computer = rand() % 3 + 1;
+
+  int user = 0;
+
+  cout << "Play Rock Paper Scissor" << endl;
+  cout << "1 = rock, 2 = paper, 3 = scissors:" << endl;
+
+  cin >> user;
+
+  bool win = false;
+
+  if (computer == user) {
+    cout << "Draw";
+  } else {
+
+    switch(user) {
+      case 1:
+        win = (computer == 2) ? false : true;
+
+      case 2:
+        win = (computer == 3) ? false : true;
+      case 3:
+        win = (computer == 1) ? false : true;
+
+    }
+  if (win == true) {
+    cout << "Great You Win!" << endl;
+  } else {
+    cout << "You Lose" << endl;
+  }
+  }
+  
 }
