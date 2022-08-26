f2fb659c067975d4e2c490e409619eba095f55fb
Yeeun888 <maximusphilipschan@gmail.com>
2022-08-26 19:01:36 +0700

    Added notes for part 1.2 - for migratory processes to macbook pro

---
+++ onlineCourseCode/learncppdotcom/chapterone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void chapter12() {
+    
+}
+
+int main() {
+    
+    //1.2 Comments
+    //Good comments are describe why some choice of code was used.
+
+    //ex: Done to see if iostream library is properly imported.
+
+    cout << "this is a test"; //This is a comment
+
+    /**
+     * @brief this is a multiline comment
+     * 
+     */
+
+    return 0;
+}
