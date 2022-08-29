90daea52000df004233b0310e02fcce87ef9f87e
Maximus Philips Chan <ner0@HP-14cm0xxx.local>
2022-08-29 18:59:00 +0700

    Finished Chapter One, Adding new notes for chapter two

--- onlineCourseCode/learncppdotcom/chapterone.cpp
+++ onlineCourseCode/learncppdotcom/chapterone.cpp
@@ -2,25 +2,69 @@
 
 using namespace std;
 
-void chapter12() {
-    
+void lastChapter() {
+    std::cout << "Enter an integer to double! -> ";
+
+    int num{};
+    std::cin >> num;
+
+    std::cout << "Double that number is " << num*2 << "!" << std::endl;
+}
+
+void chapter17() {
+    //Best naming practices for variables and functions
+
+    //Always start value names with lower case
+    /**
+     * 2. Identifiers should not being with underscore 
+     * 3. Identifiers should be descriptive to what they do in actual real code
+     */
+    int value;
+    int this_is_snake_case; //Using underscores to separate the values
+    int thisIsCamelCase;    //Using upper case letters to distinguish the values
+}
+
+void chapter16() {
+    int x;
+
+    std::cout << x;
+}
+
+void chapter15() {
+    std::cout << 4; //Display output of text or variables
+    std::cout << "Try putting a string in\n"; // "\n" is a speical character
+
+    int inputTest{};
+    cin >> inputTest;
+
+    std::cout << inputTest;
+}
+
+void chapter14() {
+               //Different types of initialization
+    int a = 3; //Copy initialization; declare and then copy a value into variable
+    int b (3); //Direct initlization; declare and initilizae together
+    int c {3};
+               //Best method because throws error for unexpected values or nonfitting values.
+}
+
+void chapter13() {
+    int x; //This is declaration, it only allocates space and a type to the variable
+    int y = 3; //This is initilization which also give it a new value
 }
 
 int main() {
     
+    std::cout << std::endl;
+
     //1.2 Comments
-    //Good comments are describe why some choice of code was used.
+    //Good comments are describe why some choice of code was used.d
 
     //ex: Done to see if iostream library is properly imported.
 
-    cout << "this is a test"; //This is a comment
+    std::cout << __cplusplus << std::endl;
 
-    /**
-     * @brief this is a multiline comment
-     * 
-     */
-
-    
+    lastChapter();
 
     return 0;
 }
